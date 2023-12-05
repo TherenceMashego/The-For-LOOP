981d89ac5821ef7d7db6951a7a123030ae5bdf5b
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 17:36:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace The_For_LOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The while loop for reference:");
+
+            int i = 0;
+            while (i <= 5)
+            {
+                Console.Write(i + " "); // Add space for better separation
+                i++;
+            }
+
+            Console.WriteLine(); // Move to the next line for clarity
+
+            Console.WriteLine("The for loop:");
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write(i + " "); // Add space for better separation
+            }
+
+            Console.ReadLine(); // Pause to see the output
+        }
+    }
+}
